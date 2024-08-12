8d9d1c5ddc8c3277b49a50fc899973d53e3e053e
issak1 <143627558+issak1@users.noreply.github.com>
2024-08-12 16:10:50 -0400

    Create BookCollection.py

---
+++ BookCollection.py
@@ -0,0 +1,7 @@
+class BookNode:
+  def __init(self, book_name), author, date_published, read=False):
+        self.book_name = book_name
+        self.author = author
+        self.date_published = date_published
+        self.read = read # indicate if a book has been read
+        self.next = None
